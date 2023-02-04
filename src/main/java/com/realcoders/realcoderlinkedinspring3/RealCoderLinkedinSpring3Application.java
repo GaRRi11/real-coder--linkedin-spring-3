330a0923ac1dcb6feb5648a44e786f4fbc24f0fc
acho01 <achosharashenidze@gmail.com>
2023-02-04 21:22:35 +0400

    Initial Commit

---
+++ src/main/java/com/realcoders/realcoderlinkedinspring3/RealCoderLinkedinSpring3Application.java
@@ -0,0 +1,13 @@
+package com.realcoders.realcoderlinkedinspring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealCoderLinkedinSpring3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealCoderLinkedinSpring3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/realcoders/realcoderlinkedinspring3/RealCoderLinkedinSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realcoders.realcoderlinkedinspring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealCoderLinkedinSpring3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
