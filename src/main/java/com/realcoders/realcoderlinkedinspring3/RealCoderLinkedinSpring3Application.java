1dd8b964ee4a3dce247ff93d9f260ffc0cde09ef
GaRRi11 <garielibekiani@gmail.com>
2023-02-04 21:51:07 +0400

    some message

--- src/main/java/com/realcoders/realcoderlinkedinspring3/RealCoderLinkedinSpring3Application.java
+++ src/main/java/com/realcoders/realcoderlinkedinspring3/RealCoderLinkedinSpring3Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RealCoderLinkedinSpring3Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //a
         SpringApplication.run(RealCoderLinkedinSpring3Application.class, args);
     }
 
